84d290c83a4f6499642f18672d5cba3300b90af8
Pavel Pavlov <ppavvvlov@gmail.com>
2023-04-02 12:56:37 +0300

    feature (PostService) добавлен свой API

--- src/API/PostService.tsx
+++ src/API/PostService.tsx
@@ -10,24 +10,27 @@ export default class PostService {
     searchValue: string,
   ) {
     const category = activeCategory > 0 ? `&category=${activeCategory}` : '';
-    const search = searchValue ? `&search=${searchValue}` : '';
-    const url = `https://63d78ad7afbba6b7c93f22b1.mockapi.io/products?page=${activePage}&limit=${limit}${category}&sortBy=${activeSort}&order=${activeSortOrder}${search}`;
+    const search = searchValue ? `&title_like=${searchValue}` : '';
+    const url = `https://react-store-api.onrender.com/products?_page=${activePage}&_limit=${limit}${category}&_sort=${activeSort}&_order=${activeSortOrder}${search}`;
     const response = await fetch(url);
     return await response.json();
   }
 
   static async getProductById(id: string) {
-    const url = `https://63d78ad7afbba6b7c93f22b1.mockapi.io/products/${id}`;
+    const url = `https://react-store-api.onrender.com/products/${id}`;
     const response = await fetch(url);
     return await response.json();
   }
 
   static async getProductsCount(activeCategory: number, searchValue: string) {
     const category = activeCategory > 0 ? `&category=${activeCategory}` : '';
-    const search = searchValue ? `&search=${searchValue}` : '';
-    const url = `https://63d78ad7afbba6b7c93f22b1.mockapi.io/products?${category}${search}`;
+    const search = searchValue ? `&title_like=${searchValue}` : '';
+    const url = `https://react-store-api.onrender.com/products?_page=1${category}${search}`;
     const response = await fetch(url);
-    const result = await response.json();
-    return result.length;
+    const totalCount = await response.headers.get('X-Total-Count');
+    if (!totalCount) {
+      return 0;
+    }
+    return +totalCount;
   }
 }
