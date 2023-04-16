86fa2daa146ab85533cb2d426111f8bacbaef060
Pavel Pavlov <ppavvvlov@gmail.com>
2023-04-16 21:22:51 +0300

    fix (MobileHeader) улучшена логика и читаемость кода burger menu с категориями в MobileHeader.

--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -3,7 +3,6 @@ import classes from './MobileHeader.module.scss';
 import Logo from '../Logo/Logo';
 import Categories from '../Categories/Categories';
 import BurgerButton from '../UI/BurgerButton/BurgerButton';
-import clsx from 'clsx';
 
 const MobileHeader = () => {
   const [menuActive, setMenuActive] = useState<boolean>(false);
@@ -11,16 +10,15 @@ const MobileHeader = () => {
     <div className={classes.mobileHeader}>
       <div className={classes.mobileHeader__row}>
         <BurgerButton menuActive={menuActive} setMenuActive={setMenuActive} />
-        <nav
-          onClick={() => {
-            setMenuActive(!menuActive);
-          }}
-          className={clsx(
-            classes.mobileHeader__categoriesMenu,
-            menuActive && classes.mobileHeader__categoriesMenu_active,
-          )}>
-          <Categories className={classes.mobileHeader__categories} buttonSize={'large'} />
-        </nav>
+        {menuActive && (
+          <nav
+            onClick={() => {
+              setMenuActive(!menuActive);
+            }}
+            className={classes.burgerMenu__container}>
+            <Categories className={classes.burgerMenu__content} buttonSize={'large'} />
+          </nav>
+        )}
         <Logo className={classes.mobileHeader__logo} />
       </div>
     </div>
